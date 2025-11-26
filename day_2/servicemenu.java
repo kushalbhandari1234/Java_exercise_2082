821daeaf7d72e1139f01db4cdba1d662958983cf
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-11-26 11:32:37 +0545

    updated

--- day_2/servicemenu.java
+++ day_2/servicemenu.java
@@ -1,25 +1,18 @@
+import java.util.Scanner;
 
+public class Servicemenu {
+    public static void main(String[] args) {
 
-public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-Scanner scanner = new scanner(System.in);
+        System.out.println("Welcome to class 11");
+        System.out.println("enter the services you require");
+        System.out.println("services we provide");
+        System.out.println("1-> software development");
+        System.out.println("2-> automation");
+        System.out.println("3-> web development");
+        System.out.println("0-> exit");
 
-System.out.println("Welcome to class 11")
-
-system.out.println ("enter the services you require")
-
-system.out.println("services we provide")
-
-system.out.println("1-> software development")
-
-system.out.println("2-> automation")
-
-system.out.println("3-> web development")
-
-system.out.println("0-> exit")
-
-scanner.close();
-}
-
-
+        scanner.close();
+    }
 }
