9e165e6a8cd197b5187cba87d312994b5ca722d4
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-12-06 14:51:44 +0545

    updated

---
+++ day_3/eligiblity.java
@@ -0,0 +1,16 @@
+package day_3;
+
+public class eligiblity {
+    public static void main (String []args)
+    {
+        int age = 2;
+        if (age >=18){
+
+            System.out.println("eligible to vote");
+        }
+
+        else{
+            System.out.println("not eligible to vote");
+        }
+    }
+}
