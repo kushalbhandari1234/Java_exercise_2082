adab4ea3df3a46b4f433a3fe1243c0ace02d0ad3
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-12-07 16:36:37 +0545

    added files

---
+++ day_4/Leapyear.java
@@ -0,0 +1,15 @@
+public class Leapyear {
+
+    public static void main(String[] args)
+
+    {
+        int year = 2030;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    
+}
