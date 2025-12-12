d71c00758f5bc7ac13c7a958d7bdf0181129c98f
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-12-12 10:46:26 +0545

    updated

---
+++ day_1/Subtract.java
@@ -0,0 +1,10 @@
+public class Subtract {
+    public static void main(String[] args) {
+        
+        int a = 33;
+        int b = 22;
+        int sub = a-b;
+        System.out.println("the sum is " +sub);
+    }
+    
+}
