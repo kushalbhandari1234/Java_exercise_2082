b5557846c64a0c0f134f67a7104be0b9e21714d6
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-12-14 15:20:46 +0545

    updated

---
+++ classwork/Numguess.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+
+public class Numguess {
+
+private int secretnumber;
+
+
+    Numguess(int number){
+secretnumber = number;
+    }
+
+void check(){
+Scanner input = new scanner (System.in);
+}
+
+   
+}
+
