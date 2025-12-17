3bb5ef0fd54703685fcff8e1b84415d8b918455f
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-12-17 11:24:10 +0545

    updated

--- classwork/NumAdv.java
+++ classwork/NumAdv.java
@@ -5,10 +5,10 @@ class NumAdv {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        int secretNumber = 93;
+        int secretNumber = 600;
         int guess, attempts = 0;
 
-        System.out.println("Guess the number (1 to 100)");
+        System.out.println("Guess the number (0 to 1000)");
 
         while (true) {
             guess = sc.nextInt();
@@ -20,6 +20,15 @@ class NumAdv {
                 break;
             }
 
+            else if (guess > secretNumber + 800) {
+                System.out.println("your number is miles away");
+                
+            }
+
+            else if (guess < secretNumber -400 ) {
+                System.out.println("your number is much more away");
+                
+            }
             if (guess < secretNumber) {
                 System.out.println("Too low");
             } else {
